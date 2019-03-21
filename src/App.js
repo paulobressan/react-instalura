f83a5145d2af901910bcc8caeba85f8eefee377b
paulo.bressan <paulo.bressan@luizalabs.com>
2019-03-21 11:52:40 -0300

    Iniciando projeto e adicionando layout base.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <header className="header container">
+        <h1 className="header-logo">
+          Instalura
+          </h1>
+
+        <form className="header-busca">
+          <input type="text" name="search" placeholder="Pesquisa" className="header-busca-campo" />
+          <input type="submit" value="Buscar" className="header-busca-submit" />
+        </form>
+
+
+        <nav>
+          <ul className="header-nav">
+            <li className="header-nav-item">
+              <a href="#">
+                {/* Quem deu like nas minhas fotos */}
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
     );
   }
 }
