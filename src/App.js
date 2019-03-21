8d26abf22a8b3eef0236f0b2270685a9ad73fcb6
paulo.bressan <paulo.bressan@luizalabs.com>
2019-03-21 17:26:10 -0300

    Criando componentes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
+import Header from './componentes/Header';
+import Timeline from './componentes/Timeline';
 
 class App extends Component {
   render() {
     return (
-      <header className="header container">
-        <h1 className="header-logo">
-          Instalura
-          </h1>
-
-        <form className="header-busca">
-          <input type="text" name="search" placeholder="Pesquisa" className="header-busca-campo" />
-          <input type="submit" value="Buscar" className="header-busca-submit" />
-        </form>
-
-
-        <nav>
-          <ul className="header-nav">
-            <li className="header-nav-item">
-              <a href="#">
-                {/* Quem deu like nas minhas fotos */}
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <div id="root">
+        <div className="main">
+          <Header />
+          <Timeline />
+        </div>
+      </div>
     );
   }
 }
